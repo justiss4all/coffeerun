50ea29f1a91b3c9ad366dc2b965c9933f40fff1c
justiss4all <bmjustiss@gmail.com>
2017-01-18 11:31:34 -0500

    Debugging

--- scripts/truck.js
+++ scripts/truck.js
@@ -15,7 +15,16 @@
     Truck.prototype.removeOrder = function(customerId) {
         console.log('Delivering order for ' + customerId);
         this.db.remove(customerId);
-    }
+    };
+
+    Truck.prototype.printOrders = function() {
+        var customerIdArray = Object.keys(this.db.getAll());
+
+        console.log('Truck #' + this.truckId + ' has pending order:');
+        customerIdArray.forEach(function(id) {
+            console.log(this.db.get(id));
+        }.bind(this));
+    };
 
     App.Truck = Truck;
     window.App = App;
